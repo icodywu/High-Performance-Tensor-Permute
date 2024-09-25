33a18b79ba73c270eac1addc7919b03b7f8fc055
icodywu <125686483+icodywu@users.noreply.github.com>
2024-09-25 09:15:04 -0700

    Update permute.h

--- permute.h
+++ permute.h
@@ -5,14 +5,12 @@
 #include <stdint.h>
 #include <limits.h>
 
-int permute(const void* src, void* dst, uint64_t dtypeSize, uint64_t* src_dims,
-    uint64_t src_ndim, uint64_t* permute_idx, uint64_t* dst_dims, int nThreads = 1);
 /**
  * @brief This file provides an optimized tensor permutation method, under the function Permute()
  * It contains 4 optimization steps.
- * Step 1. Sqeeze the trivial dimensions of width = 1. It takes O(n) time and space complexity.
- * EX.   perm_idx[2, 3, 5, 4, 1, 7, 0,  6]  ==>  [1, 2, 3, 0, 5, 4]
- *       src_nums[8, 9, 1, 6, 4, 6, 1, 10]  ==>  [8, 9, 6, 4, 6, 10]
+ * Step 1. Squeeze the trivial dimensions of width = 1. It takes O(n) time and space complexity.
+ * EX.   perm_idx[2, 3, 5, 4, 1, 7, 0,  6]  ==>  [2, 4, 3, 1, 6, 0,  5]
+ *       src_nums[8, 9, 1, 6, 4, 6, 9, 10]  ==>  [8, 9, 6, 4, 6, 9, 10]
  *
  * Step 2. Compress the consecutive permuted dimensions. It takes O(n) time and space complexity.
  * EX.   perm_idx[2, 3,  4,  5,    0, 1,   6, 7 ]  ==>  [1,           0,    2 ]
@@ -24,20 +22,30 @@ int permute(const void* src, void* dst, uint64_t dtypeSize, uint64_t* src_dims,
  *       trim_dims[20, 8, 16, 12, 7]   ==> trim_dims[20, 8, 16, 12]
  *       dtypeSize = 1                 ==> dtypeSize = 7
  *
- * Step 4. Apply the proposed generalized batch transpose over the case perm_idx[ndim-1] != ndim-1;
- *         Apply memcpy() to move the entire last dim of data over the case perm_idx[ndim-1] == ndim-1.
- *         It is worth noting that both operations involve nearly no multimplication and completely no division to compute permuted indexes
- *            as opposed to the straightforword method.
+ * Step 4. Case 1. The last dim is permuted, i.e., perm_idx[ndim-1] != ndim-1.
+ *   Fundamentally, such permutation can be viewed as a generalized transpose. We thus propose an innovative generalized batch transpose technique,
+ *   which effectively takes advantage of the cache line by creating column-wise consecutive write addresses.
+ *   For special dtypeSize in {3, 5, 6, 7} (which is created from the merging of the small last dim), the data movement in an overlapped manner. 
+ *   The details are provided in
+ *   void Generalized_Transpose(const void *src, void *dst, uint64_t srcOffset, uint64_t dstOffset, const G_Trans_Param gtrans)
  *
- * By default, it utilizes single thread. Multi-thread is activated by setting the parameter nThreads >1.
- * By partitioning evenly along the first dim, i.e., [0], the proposed multi-thread operations literally linearly reduces the running time,
+ *   Case 2. The last dim is unpermuted, i.e., perm_idx[ndim-1] == ndim-1. 
+ *   In this case, we deploy memcpy() to move the entire last dim of data (recall the last dim size is coerced to be greater than 8B). 
+ *   The details are given in
+ *   void Permute_TypeB_Kernel(const void* src, void* dst, uint64_t dtypeSize, const uint64_t src_ndim, uint64_t* src_dims, uint64_t* src_wt, uint64_t* dst_wt);
+ *
+ * By default, it utilizes a single thread. Multi-thread is activated by setting the parameter nThreads >1.
+ * By partitioning evenly along the first dim, i.e., [0], the proposed multi-thread operations nearly linearly reduce the running time,
  * until it saturates the memory bandwidth.
  */
+int permute(const void* src, void* dst, uint64_t dtypeSize, uint64_t* src_dims,
+    uint64_t src_ndim, uint64_t* permute_idx, uint64_t* dst_dims, int nThreads = 1);
+
 
  /* permute_validation() uses the randomly generated tensors and permutations to test against the equality
  *  permute(tensor, perm_idx) -> permuted_tensor
  *  permute(permuted_tensor, perm_inv) = tensor, where perm_inv is the inverse array of perm_idx.
- *  The first set of tensor dimension and perm_idx is hand designed to test a particular corner case
+ *  The first set of tensor dimensions and perm_idx is hand-designed to test a particular corner case
  *  whereas the remaining test cases are randomly generated to expand coverage. 
  */
 void permute_validation();
